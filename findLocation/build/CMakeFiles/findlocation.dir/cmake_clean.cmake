1cfa5586fc724d147125b7210ed81ea4c7f250b2
user <chris.henderson1999@gmail.com>
2020-11-16 19:58:04 +0400

    Added find location and look at location files from lab 3

---
+++ findLocation/build/CMakeFiles/findlocation.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/findlocation.dir/findLocation.cpp.o"
+  "findlocation.pdb"
+  "findlocation"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/findlocation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ lookAtLocation/build/CMakeFiles/lookatlocation.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lookatlocation.dir/lookAtLocation.cpp.o"
+  "lookatlocation.pdb"
+  "lookatlocation"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lookatlocation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
