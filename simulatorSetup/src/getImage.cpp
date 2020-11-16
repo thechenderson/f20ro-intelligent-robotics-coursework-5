52f8991a82c22ce3acd713fa9be94fa4680bc29f
Christopher Henderson <chris.henderson1999@gmail.com>
2020-11-16 14:25:49 +0000

    Added basic reasing and writing images code

---
+++ simulatorSetup/src/getImage.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/* Get all OS and signal processing YARP classes */
+#include <yarp/os/all.h>
+#include <yarp/sig/all.h>
+using namespace yarp::os;
+using namespace yarp::sig;
+int main() {
+ Network yarp; // set up yarp
+ // make a port for reading images
+ BufferedPort<ImageOf<PixelRgb> > imagePort;
+imagePort.open("/tutorial/image/in"); // give the port a name
+ while (1) {
+ ImageOf<PixelRgb> *image = imagePort.read(); // read an image
+ if (image!=NULL) { // check we actually got something
+ printf("We got an image of size %dx%d\n", image->width(), image-
+>height());
+ }
+ }
+ return 0;
+}
