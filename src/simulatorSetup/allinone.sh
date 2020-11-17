04727e7c37adf5dca7879064da955d214bb97054
Christopher Henderson <chris.henderson1999@gmail.com>
2020-11-17 19:52:30 +0000

    Attempt at auto closing terminals again

--- src/simulatorSetup/allinone.sh
+++ src/simulatorSetup/allinone.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-gnome-terminal -- bash -c "yarpserver; exec bash"
-gnome-terminal -- bash -c "iCub_SIM; exec bash"
-gnome-terminal -- bash -c "yarpview -name /view/left; exec bash"
+gnome-terminal -- .bin/bash -c "yarpserver; exec bash"
+gnome-terminal -- /bin/bash -c "iCub_SIM; exec bash"
+gnome-terminal -- /bin/bash -c "yarpview -name /view/left; exec bash"
 sleep 1
-gnome-terminal -- bash -c "yarp connect /icubSim/cam/left /yarpview/img:i;"
+gnome-terminal -- /bin/bash -c "yarp connect /icubSim/cam/left /yarpview/img:i;"
 sleep 1
-gnome-terminal -- bash -c "yarpdev --device opencv_grabber --name /test/video; exec bash"
+gnome-terminal -- /bin/bash -c "yarpdev --device opencv_grabber --name /test/video; exec bash"
 sleep 2
-gnome-terminal -- bash -c "yarp connect /test/video /icubSim/texture/screen;"
+gnome-terminal -- /bin/bash -c "yarp connect /test/video /icubSim/texture/screen;"
