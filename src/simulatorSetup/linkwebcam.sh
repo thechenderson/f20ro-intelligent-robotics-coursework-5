e21f0e2c67943fc0715848658554047f536a13f7
Christopher Henderson <chris.henderson1999@gmail.com>
2020-11-17 16:00:19 +0000

    Fixed broken webcam link script

--- src/simulatorSetup/linkwebcam.sh
+++ src/simulatorSetup/linkwebcam.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-gnome-terminal -- bash -c "yarpdev --device opencv_grabber --name /test/video
-; exec bash"
+gnome-terminal -- bash -c "yarpdev --device opencv_grabber --name /test/video; exec bash"
 gnome-terminal -- bash -c "yarp connect /test/video /icubSim/texture/screen; exec bash"
 
