879e19db90764d8100995ae95186ed52aef4f30a
Christopher Henderson <chris.henderson1999@gmail.com>
2020-11-21 11:10:47 +0000

    Re-arranged files

---
+++ .bash_history
@@ -0,0 +1,7 @@
+mount vboxsf f20ro-intelligent-robotics-coursework-5 /home/user
+sudo mount vboxsf f20ro-intelligent-robotics-coursework-5 /home/user
+sudo mount.vboxsf f20ro-intelligent-robotics-coursework-5 /home/user
+ls
+umount f20ro-intelligent-robotics-coursework-5/
+umount f20ro-intelligent-robotics-coursework-5
+sudo mount.vboxsf f20ro-intelligent-robotics-coursework-5 /home/user/
---
+++ src/examplePrograms/lookAtLocation/build/CMakeFiles/lookatlocation.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/user/labs/lab3/look_at_location/lookAtLocation.cpp" "/home/user/labs/lab3/look_at_location/build/CMakeFiles/lookatlocation.dir/lookAtLocation.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/user/software/git/yarp/src/libYARP_OS/include"
+  "/home/user/software/git/yarp/build/src/libYARP_conf/include"
+  "/home/user/software/git/yarp/src/libYARP_sig/include"
+  "/home/user/software/git/yarp/src/libYARP_dev/include"
+  "/home/user/software/git/yarp/src/libYARP_math/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
