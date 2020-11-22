73e054a62c976eba46dd8f7d1a3c803908cdb3a0
Christopher Henderson <chris.henderson199@gmail.com>
2020-11-22 14:08:15 +0000

    Added comments to allinone script and added empty script to build all projects at once

--- src/simulatorSetup/allinone.sh
+++ src/simulatorSetup/allinone.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
+
+#Start yarpserver
 gnome-terminal -- /bin/bash -c "yarpserver; exec bash"
+
+#Start iCubSim
 gnome-terminal -- /bin/bash -c "iCub_SIM; exec bash"
+
+#Create a new yarpview for robot's perspective
 gnome-terminal -- /bin/bash -c "yarpview -name /view/left; exec bash"
 sleep 1 
 gnome-terminal -- /bin/bash -c "yarp connect /icubSim/cam/left /yarpview/img:i;"
 sleep 1 
+
+#Set up virtual simulator screen to display video from webcam
 gnome-terminal -- /bin/bash -c "yarpdev --device opencv_grabber --name /test/video; exec bash"
 sleep 2 
 gnome-terminal -- /bin/bash -c "yarp connect /test/video /icubSim/texture/screen;"
-sleep 1 
+sleep 1
+
+#Apply linear filter to new yarpview
 gnome-terminal -- /bin/bash -c "./../applyLinearFilter/build/applyLinearFilter"
 gnome-terminal -- /bin/bash -c "yarpview -name /view/Sobel_edges; exec bash"
 sleep 1 
---
+++ src/simulatorSetup/buildall.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#Script used to build all of the C++ files before use in the simulator
+
+
+gnome-terminal -- /bin/bash -c "cd ../applyLinearFilter/build;cmake ..; exec bash"
