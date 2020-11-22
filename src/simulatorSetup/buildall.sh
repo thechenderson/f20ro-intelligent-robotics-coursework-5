0b69917098136792be03e57df64f78609f95efa5
Christopher Henderson <chris.henderson199@gmail.com>
2020-11-22 17:40:12 +0000

    Renamed scripts so less typing required in terminal (lazy)

--- src/simulatorSetup/buildall.sh
+++ src/simulatorSetup/buildall.sh
@@ -2,4 +2,4 @@
 #Script used to build all of the C++ files before use in the simulator
 
 
-gnome-terminal -- /bin/bash -c "mkdir ../applyLinearFilter/build ; cd ../applyLinearFilter/build ; cmake ..; mkdir ../../arMarkers/build ; cd ../../arMarkers/build ; cmake .. ; exec bash"
+gnome-terminal -- /bin/bash -c "mkdir ../applyLinearFilter/build ; cd ../applyLinearFilter/build ; cmake ..; mkdir ../../arMarkers/build ; cd ../../arMarkers/build ; cmake .. ; mkdir ../../faceDetection/build ; cd ../../faceDetection/build ; cmake .. ; exec bash"
