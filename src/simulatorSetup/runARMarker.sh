31b0a43787ce48e49173e3ac835f1e3196bc84eb
Christopher Henderson <chris.henderson199@gmail.com>
2020-11-22 17:35:34 +0000

    Added skeleton scripts to run ar marker detection and face recognition

---
+++ src/simulatorSetup/runARMarker.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#Start yarpserver
+gnome-terminal -- /bin/bash -c "yarpserver; exec bash"
+
+#Start iCubSim
+gnome-terminal -- /bin/bash -c "iCub_SIM; exec bash"
+
+#Create a new yarpview for robot's perspective
+gnome-terminal -- /bin/bash -c "yarpview --name /view/left; exec bash"
+sleep 1 
+gnome-terminal -- /bin/bash -c "yarp connect /icubSim/cam/left /view/left;"
+sleep 1 
+
+#Set up virtual simulator screen to display video from webcam
+gnome-terminal -- /bin/bash -c "yarpdev --device opencv_grabber --name /webcam/video; exec bash"
+sleep 2 
+gnome-terminal -- /bin/bash -c "yarp connect /webcam/video /icubSim/texture/screen;"
+sleep 1
+
+#Apply ar marker search on new yarpview
+gnome-terminal -- /bin/bash -c "./../arMarkers/build/arMarkers"
+gnome-terminal -- /bin/bash -c "yarpview --name /view/arMarkers; exec bash"
+sleep 2
+gnome-terminal -- /bin/bash -c "yarp connect /webcam/video /imageProc/detectMarker/in;"
+sleep 2
+gnome-terminal -- /bin/bash -c "yarp connect /imageProc/detectMarker/out /view/detectMarker;"
---
+++ src/simulatorSetup/runLinearFilter.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#Start yarpserver
+gnome-terminal -- /bin/bash -c "yarpserver; exec bash"
+
+#Start iCubSim
+gnome-terminal -- /bin/bash -c "iCub_SIM; exec bash"
+
+#Create a new yarpview for robot's perspective
+gnome-terminal -- /bin/bash -c "yarpview --name /view/left; exec bash"
+sleep 1 
+gnome-terminal -- /bin/bash -c "yarp connect /icubSim/cam/left /view/left;"
+sleep 1 
+
+#Set up virtual simulator screen to display video from webcam
+gnome-terminal -- /bin/bash -c "yarpdev --device opencv_grabber --name /webcam/video; exec bash"
+sleep 2 
+gnome-terminal -- /bin/bash -c "yarp connect /webcam/video /icubSim/texture/screen;"
+sleep 1
+
+#Apply linear filter to new yarpview
+gnome-terminal -- /bin/bash -c "./../applyLinearFilter/build/applyLinearFilter"
+gnome-terminal -- /bin/bash -c "yarpview --name /view/Sobel_edges; exec bash"
+sleep 2
+gnome-terminal -- /bin/bash -c "yarp connect /webcam/video /imageProc/edgeDetection/in;"
+sleep 2
+gnome-terminal -- /bin/bash -c "yarp connect /imageProc/edgeDetection/out /view/Sobel_edges;"
