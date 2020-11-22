4d8eb48b31cf15e8e5cf276f4794b911fac4660b
Christopher Henderson <chris.henderson199@gmail.com>
2020-11-22 14:45:37 +0000

    Updated .gitignore for arMarkers build files and added buildall script to compile all files at once

--- src/simulatorSetup/buildall.sh
+++ src/simulatorSetup/buildall.sh
@@ -2,4 +2,4 @@
 #Script used to build all of the C++ files before use in the simulator
 
 
-gnome-terminal -- /bin/bash -c "cd ../applyLinearFilter/build;cmake ..; exec bash"
+gnome-terminal -- /bin/bash -c "cd ../applyLinearFilter/build ; cmake ..; cd ../../arMarkers/build ; cmake .. ; exec bash"
