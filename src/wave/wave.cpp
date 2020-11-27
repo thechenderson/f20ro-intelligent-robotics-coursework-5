58e06df5be27b5b3a52f85e6493e5f67bb3c9a11
ad2015-hw <74624889+ad2015-hw@users.noreply.github.com>
2020-11-27 17:49:02 +0400

    Add files via upload

---
+++ src/wave/wave.cpp
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <yarp/os/all.h>
+#include <yarp/sig/all.h>
+#include <yarp/dev/all.h>
+
+using namespace yarp::os;
+using namespace yarp::sig;
+using namespace yarp::dev;
+
+int main(int argc, const char **argv)
+{
+
+	Network yarp;
+	
+	bool MarkerDetection = true;
+	
+	//Options of right arm
+	Property options; 
+	options.put("device", "remote_controlboard");
+	options.put("local", "/tutorial/client");
+	options.put("remote", "/icubSim/right_arm");
+
+	PolyDriver robotDev(options);
+	if (!robotDev.isValid())
+	{
+	  printf("Device not available.\n");
+	  return 1;
+	}
+
+	//Create object to get the data from the right arm
+	IPositionControl *pos;
+	IEncoders *enc;
+	IControlMode2 *ictrl;
+	robotDev.view(pos);
+	robotDev.view(enc);
+	robotDev.view(ictrl);
+	//Values that will be sent and received
+	yarp::sig::Vector setpoints;
+	yarp::sig::Vector Encoders;
+
+	//Getting joints number of right arm
+	int joints = 0;
+	setpoints.resize(joints);
+	pos->getAxes(&joints);
+	
+	//Resize vector with the number of joints in the arm
+	setpoints.resize(joints);
+	Encoders.resize(joints);
+	
+	//Modify every joint with parameters
+	for (int i = 0; i < joints; i++)
+	{
+	  ictrl->setControlMode(i, VOCAB_CM_POSITION); //set right hand joints in position mode
+	  setpoints[i] = 0;
+	}
+
+	//Set joints to raise the arm to the vertical
+	pos->positionMove(0, -90);
+	pos->positionMove(1, 90);
+
+	while(MarkerDetection) {
+		//Motion of wave (not the Queen's one)
+		if (Encoders[3] >= 80)
+		{
+		  pos->positionMove(3, 40);
+		}
+		if (Encoders[3] <= 50)
+		{
+		  pos->positionMove(3, 90);
+		}
+		//Update values of the encoders
+		enc->getEncoders(Encoders.data());
+	}
+}
